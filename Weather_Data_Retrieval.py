4dc2a32ef34b51092c0e6e83d3dde55c53337c7c
Daniel <danieleberhart14@gmail.com>
2020-09-21 20:47:46 -0700

    Importing existing weather data retrieval program.
    This program was already completed. This commit is simply to import it into Github.

---
+++ Weather_Data_Retrieval.py
@@ -0,0 +1,24 @@
+# Weather Data Retrieval Program
+from wwo_hist import retrieve_hist_data
+import urllib.request
+import os
+
+# Destination directory
+os.chdir("F:\\Daniels Stuff\\Coding Stuff\\Plant recommendation")
+
+# Setting up parameters for the excel file.
+# Code taken from https://pypi.org/project/wwo-hist/
+frequency=24
+start_date = '11-DEC-2010'
+end_date = '01-AUG-2020'
+api_key = '1354d0c9698c486ea3e191604200408'
+location_list = ['97603']
+
+hist_weather_data = retrieve_hist_data(api_key,
+                                location_list,
+                                start_date,
+                                end_date,
+                                frequency,
+                                location_label = False,
+                                export_csv = True,
+                                store_df = True)
