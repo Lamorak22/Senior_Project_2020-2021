545ecb8f6359356855dccf7c94853ae8add5170e
Daniel <danieleberhart14@gmail.com>
2020-11-20 13:27:09 -0800

    Slight changes to make code more understandable/readable

--- Plant_Recommendation.py
+++ Plant_Recommendation.py
@@ -1,3 +1,6 @@
+# Plant Recommendation Program for Senior Project 2020-2021
+# Daniel Eberhart
+
 import pandas as pd
 
 #Find the date within the dataframe
@@ -13,11 +16,11 @@ def findDate(index, date):
         else:
             index += 1
 
-def findAvgTempC(index_list, plant_time_days, tmp, avg_temp, cnt):
+def findAvgTempC(index_list, plant_time_days, tempC, avg_temp, cnt):
     # Get the total temperature from each index for the date
     for x in index_list:
         for i in range(x, x + int(plant_time_days[cnt])):
-            avg_temp += tmp[i]
+            avg_temp += tempC[i]
     return avg_temp/(plant_time_days[cnt]*len(index_list))
 
 
@@ -28,10 +31,10 @@ date = "4/1"
 avg_temp = 0
 total_days = 0
 
-# Read the file for weather data
+# Read the file for weather data and put into dataframe
 df = pd.read_csv("F:\\Daniels Stuff\\Coding Stuff\\Plant recommendation\\97603.csv")#.set_index('date_time')
-date_t = df['date_time'] # List of dates
-tmp = df['tempC'] # Average temperature for each date
+date_t = df['date_time'] # Variable for list of dates
+tempC = df['tempC'] # Variable for average temperature at each date
 
 # Read the plant database
 df2 = pd.read_excel("F:\\Daniels Stuff\\Coding Stuff\\Plant recommendation\\Plant_Database.xlsx", skiprows=0)
@@ -41,17 +44,18 @@ growth_time = df2['growth_time_days'] # Growth time for each plant
 
 # Parse the database to find the plants in the "to_plant_list"
 to_plant_list = [None] * len(plant)
-min_tempC_list = [None] * len(plant)
-plant_time_days = [None] * len(plant)
+min_tempC_list = [None] * len(min_tempC)
+plant_time_days = [None] * len(growth_time)
 
 
+# Transfer data from pandas over to list format from "class 'pandas.core.series.Series'"
 for x in range(0, len(plant)):
     to_plant_list[x] = plant[x]
     plant_time_days[x] = growth_time[x]
     min_tempC_list[x] = min_tempC[x]
 
 
-# Find all of the indexes of selected date
+# Find all of the indexes of selected date from 2010 to 2020
 index_list = [0,0,0,0,0,0,0,0,0,0] 
 for x in range(0,len(index_list)):
     index = findDate(index, date)
@@ -64,7 +68,7 @@ for x in range(0,len(index_list)):
 temp = 0
 for x in range(0, len(to_plant_list)):
     avg_temp = 0
-    temp = findAvgTempC(index_list, plant_time_days, tmp, avg_temp, x)
+    temp = findAvgTempC(index_list, plant_time_days, tempC, avg_temp, x)
     temp = int(temp) #temp is not int. Needs to be an int for if statement
     if temp > int(min_tempC_list[x]):
         print(f"Average temperature is: {temp}, {to_plant_list[x]}'s minimum temperature is {min_tempC_list[x]}, so it is good to plant")
