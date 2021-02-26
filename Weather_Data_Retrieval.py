6a43765489e21a8c33cf187ef70bcc2e9623ac40
Daniel <danieleberhart14@gmail.com>
2021-02-26 11:13:13 -0800

    Update Weather_Data_Retrieval.py

--- Weather_Data_Retrieval.py
+++ Weather_Data_Retrieval.py
@@ -1,18 +1,19 @@
-# Weather Data Retrieval Program
+#Weather Data Retrieval Program
+
 from wwo_hist import retrieve_hist_data
+#from wwo_hist import hist
 import urllib.request
 import os
 
-# Destination directory
-os.chdir("F:\\Daniels Stuff\\Coding Stuff\\Plant recommendation")
 
-# Setting up parameters for the excel file.
-# Code taken from https://pypi.org/project/wwo-hist/
+os.chdir("F:\\Daniels Stuff\\Coding Stuff\\Plant recommendation\\temp")
+
 frequency=24
 start_date = '01-JAN-2010'
-end_date = '27-SEP-2020'
-api_key = '' 
+end_date = '01-JAN-2021'
+api_key = '0909c9292f294476aba41920211701'
 location_list = ['97603']
+zipcode = "97603"
 
 hist_weather_data = retrieve_hist_data(api_key,
                                 location_list,
@@ -22,3 +23,8 @@ hist_weather_data = retrieve_hist_data(api_key,
                                 location_label = False,
                                 export_csv = True,
                                 store_df = True)
+
+if os.path.isfile(f"F:\\Daniels Stuff\\Coding Stuff\\Plant recommendation\\temp\\{zipcode}.csv"):
+    print("Exists")
+else:
+    print("does not exist")
