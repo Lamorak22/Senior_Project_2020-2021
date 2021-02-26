dac0df77a81f7e693f0988a73a055144047d890c
Daniel <danieleberhart14@gmail.com>
2021-02-26 11:13:01 -0800

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,87 +1,143 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import Calendar from 'react-calendar'
-import './App.css';
-import { fetch_sensor_data } from './app_client.js';
-import { callPRP } from './prp.js';
+import './App.css'
 import logo from './plant.png'
+import loadgif from './plantgif.gif'
+import CurrTime from './currTime'
 
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
 import Image from 'react-bootstrap/Image'
 import Form from 'react-bootstrap/Form'
 
-import CurrTime from './currTime'
-import { getWeatherData } from './weatherAPI';
-
+import { fetch_sensor_data } from './app_client.js';
+import { callPRP } from './prp.js';
+import { waterGarden } from './waterGarden.js'
 
 
 
 function App() {
   var caldate;
 
-  //Calendar
+  //useState to update the date on the calendar per user input
   const [calendarVal, changeDate] = useState(new Date());
 
-  const [inputZipcode, updateZip] = useState("")
+  //Get Zipcode from text box
 
+    /********************************************************************
+   * Function: updateField
+   * 
+   * Purpose: When the user types into the zipcode box, the function
+   *  will update the data in the state variable
+   * 
+   * input: user keyboard input
+   * 
+   * output: None
+  **************************************************************/
+  const [inputZipcode, updateZip] = useState("")
   function updateField(event) {
-      console.log(event.nativeEvent.data);
-      updateZip(inputZipcode + event.nativeEvent.data)
+      //console.log(event.target.value);
+      updateZip(event.target.value)
     };
 
 
-  //PRP
-  const [prpdata, setprpdata] = useState({});
+  /********************************************************************
+   * Function: clickHandler
+   * 
+   * Purpose: Once a date and zipcode are selected/entered, this function
+   *  will call the plant recommendation program from the Flask API
+   * 
+   * input: selected date and zipcode
+   * 
+   * output: array of vegetables that were deemed "good to plant"
+  **************************************************************/
 
-  const [weatherdata, setweatherdata] = useState({});
+    //Data for plant recommendation program
+    const [prpdata, setprpdata] = useState("");
 
-  function clickHandler(){
+  async function clickHandler(){
+      
+      caldate = calendarVal.toLocaleDateString();
+      const data = await callPRP(caldate, inputZipcode);
+      console.log("data: ", data);
 
-    getWeatherData().then((data) => {
-      console.log(data.data.weather[0]);
-      setweatherdata(data);
-      console.log(weatherdata.data.weather[0])
-  })
-
-    caldate = calendarVal.toLocaleDateString();
-    callPRP(caldate, inputZipcode, weatherdata.data.weather[0]).then((data) => {
-      setprpdata(data);
-    })
-    
-    
-
-
+      //Format prpdata
+      var i;
+      var prpresults = "";
+      for (i = 0; i < data.prp.length; i++){
+        if (data.prp.length - 1 === i){
+          prpresults += "and " + data.prp[i];
+        }
+        else {
+          prpresults += data.prp[i] + ", ";
+        }  
+      }
+      prpresults += " are all options to plant based on the historical weather data of the date chosen."
+      setprpdata(prpresults);
   }
+
 
   //GET request for sensor data
   const [stateData, setStateData] = useState({});
     useEffect(() => {
+      setLoading(loadgif);
       fetch_sensor_data().then((data) => {
         setStateData(data);
       });
       }, []);
+  
       
+    /********************************************************************
+   * Function: waterHandler
+   * 
+   * Purpose: This function will be called at a certain time each day.
+   *  When it is called, a request will be made to the Flask API
+   *  to open the solenoid valve and water the garden.
+   * 
+   * input: Time of day
+   * 
+   * output: Will water the garden with solenoid valve.
+  **************************************************************/
+ useEffect(() => {
+  const timer = setInterval(() =>{
+    caldate = calendarVal.toLocaleTimeString();
+    console.log(caldate[0]);
+    var ampm = caldate.slice(-2)
+    console.log(ampm)
+    if (caldate[0] === "9" && ampm === "PM"){
+      waterHandler();
+    }
+  }, 60000);
+
+  return () => clearInterval(timer);
+ });
+
+  function waterHandler(){
+    waterGarden().then((data) => {
+      console.log(data);
+    });
+  }
   
-  
+
+  const [loading, setLoading] = useState({});
+ 
+
   return (
   <div className="App">
-    <div class="wrappingimage">
+
+    {/*Flex box for the plant image and other webpage headers*/}
+    <div className="wrappingimage">
       <Image src={logo} alt="Plant" class="img-fluid" width="396" height="395"/>
-      
+      <Image src={loading} alt="Plantgif" class="img-fluid" width="396" height="395"/>
+      <div className="imagetext">
+      <div>Automated Garden Webpage</div>
+      <div>Creator: Daniel Eberhart</div>
+      <CurrTime></CurrTime>
       </div>
-      <p>Automated Garden Webpage<br/>
-        Creator: Daniel Eberhart
-      </p>
+    </div>     
       
-      <h2>Sensor Table</h2>
-      <h2>Last updated: {stateData.date}</h2>
-      
-    <CurrTime></CurrTime>
-    <div>
-    <Button variant="primary" size="sm">
-      Refresh Sensor Data
-    </Button>{' '}
-    </div>
+      {/*Table to hold all sensor data retrieved from Flask API*/}
+    <h2>Sensor Table</h2>
   <div>
     <Table striped bordered hover variant="dark" size="sm">
       <thead>
@@ -108,7 +164,7 @@ function App() {
 
         </tr>
         <tr>
-          <td>Humididty Sensor</td>
+          <td>Humidity Sensor</td>
           <td>{stateData.humidity}</td>
 
         </tr>
@@ -116,23 +172,19 @@ function App() {
     </Table>
   </div>
 
+    {/*Flex box that holds all assets of the plant recommendation program*/}
+    <h2>Plant Recommendation Program</h2>
 
-  <div class="calendarwrapper">
+  <prp>
+    <div class="calendar">
     <Calendar
       onChange={changeDate}
       value={calendarVal}
       /> 
-    <Button 
-    variant="secondary"
-    size="sm"
-    onClick={clickHandler}
-    >
-      Submit
-    </Button> 
-    <p>prp data:{prpdata?.data}</p>
-  </div>
+    </div>
 
-    <div class="text_entry">
+
+    <div class="textentry">
     <Form>
       <Form.Group>
       <Form.Control 
@@ -140,14 +192,70 @@ function App() {
       value={inputZipcode}
       onChange={updateField}
       />
-      
-      
       </Form.Group>
     </Form>
 
-      <p>form: {inputZipcode}</p>
+    <Button 
+      variant="secondary"
+      size="lg"
+      onClick={clickHandler}
+      >
+        Submit
+      </Button>
     </div>
 
+
+    <div class="prpoutput">
+      <p>{prpdata}</p>
+    </div>
+
+    <div class="chooseplant">
+      <Form>
+        <Form.Group>
+          <Form.Label column="lg">Select what to plant </Form.Label>
+          <Form.Control as="select" size="sm" custom>
+            <option value="0">Basil</option>
+            <option value="1">Beet</option>
+            <option value="2">Beetroot</option>
+            <option value="3">Bell Pepper</option>
+            <option value="4">Broccoli</option>
+            <option value="5">Cabbage</option>
+            <option value="6">Carrot</option>
+            <option value="7">Cauliflower</option>
+            <option value="8">Celery</option>
+            <option value="9">Cucumber</option>
+            <option value="10">Green Bean</option>
+            <option value="11">Kale</option>
+            <option value="12">Lettuce</option>
+            <option value="13">Mint</option>
+            <option value="14">Onion</option>
+            <option value="15">Pea</option>
+            <option value="16">Potato</option>
+            <option value="17">Radish</option>
+            <option value="18">Red Pepper</option>
+            <option value="19">Romaine</option>
+            <option value="20">Spinach</option>
+            <option value="21">Squash</option>
+            <option value="22">Tomato</option>
+            <option value="23">Zucchini</option>
+          </Form.Control>
+        </Form.Group>
+      </Form>
+
+
+    </div>
+  </prp>
+
+    
+    <div>
+    <Button 
+      variant="secondary"
+      size="sm"
+      onClick={waterHandler}
+      >
+        WATER
+      </Button>
+    </div>
     
   </div>
   );
